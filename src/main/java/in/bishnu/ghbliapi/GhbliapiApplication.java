f09adac437866c192d8cede10286c84b45394b97
BishnuBhakat <bhakatbishnu8@gmail.com>
2025-06-24 18:05:25 +0530

    done808

--- src/main/java/in/bishnu/ghbliapi/GhbliapiApplication.java
+++ src/main/java/in/bishnu/ghbliapi/GhbliapiApplication.java
@@ -3,6 +3,9 @@ package in.bishnu.ghbliapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -12,4 +15,17 @@ public class GhbliapiApplication {
 		SpringApplication.run(GhbliapiApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer webMvcConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:5173", "https://ghibli-api-frontend.vercel.app")
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+						.allowedHeaders("*")
+						.allowCredentials(true);
+			}
+		};
+	}
 }
